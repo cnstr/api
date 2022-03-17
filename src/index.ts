517f08af9da6ab3e4533a9f6ca1cb5bf89e62af4
Aarnav Tale <aarnav@tale.me>
2022-03-17 12:54:18 -0400

    fix: don't add handlers on the main server

--- src/index.ts
+++ src/index.ts
@@ -13,29 +13,7 @@ declare global {
 	const API_ENDPOINT: string
 }
 
-const server = new App({
-	noMatchHandler: (_req, res) => {
-		return res.status(404).json({
-			message: '404 Not Found',
-			date: new Date()
-		})
-	},
-
-	onError: async (err, _req, res) => {
-		// TODO: Sentry capture
-		console.log(err)
-		return res.status(500).json({
-			message: '500 Internal Server Error',
-			date: new Date()
-		})
-	},
-
-	settings: {
-		xPoweredBy: 'Argo'
-	}
-})
-
-
+const server = new App()
 export const http = new App({
 	noMatchHandler: (_req, res) => {
 		return res.status(404).json({
