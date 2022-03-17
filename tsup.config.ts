bf785ec46191b8bbb4897255f76f44c82476a57b
Aarnav Tale <aarnav@tale.me>
2022-03-16 23:12:06 -0400

    feat: add preprocessor macros

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'tsup'
+import simpleGit from 'simple-git'
+import { stat } from 'node:fs/promises'
 
 const header =
 `// ---------------------------------------------------
@@ -6,8 +8,31 @@ const header =
 // See the attached LICENSE file for more information.
 // ---------------------------------------------------`
 
+const git = simpleGit('.')
+
+const commit = await git.revparse('HEAD')
+const tag = await git.tag(['--sort=committerdate'])
+
+const stats = await stat('dist/index.js')
+const modified = new Date(stats.mtime)
+const modifiedString = `${modified.getFullYear()}.${modified.getMonth()}.${modified.getDate()}`
+
+const replacements = {
+	'GIT_COMMIT': `"${commit}"`,
+	'RELEASE_VERSION': `"${tag.trim()}"`,
+	'FILE_MODIFIED': `"${modifiedString}"`,
+	'API_ENDPOINT': '"https://api.canister.me/v2"'
+}
+
+console.log('Using the following replacements: %s', replacements)
+console.log()
+
 export default defineConfig((options) => {
 	return {
+		esbuildOptions(options) {
+			options.define = replacements
+		},
+
 		entry: ['./src/index.ts'],
 		target: 'node16',
 		splitting: false,
