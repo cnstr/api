7a98bd7f15be59b738721175891a2ce37b15cdae
Aarnav Tale <aarnav@tale.me>
2022-09-03 13:41:34 -0400

    feat: implement multi package lookup

---
+++ src/router/package/multiLookup.ts
@@ -0,0 +1,69 @@
+import { Package } from '@canister/models'
+import type { NextFunction, Request, Response } from '@tinyhttp/app'
+import { database } from 'database.js'
+import { Brackets } from 'typeorm'
+
+type LookupResponse = Response & {
+	locals: {
+		query: string[];
+	};
+}
+
+export function middleware(request: Request, response: LookupResponse, next: NextFunction) {
+	const query = request.query.ids
+	if (!query) {
+		return response.status(400)
+			.json({
+				message: '400 Bad Request',
+				error: 'Missing URL parameter: \':packages\'',
+				date: new Date()
+			})
+	}
+
+	response.locals.query = query.toString()
+		.split(',')
+	next()
+}
+
+export async function handler(_request: Request, response: LookupResponse) {
+	const { query } = response.locals
+	const pkgs = await database.createQueryBuilder(Package, 'p')
+		.select()
+		.leftJoinAndSelect('p.repository', 'repository')
+		.where({
+			isCurrent: true,
+			isPruned: false
+		})
+		.andWhere(new Brackets(qb => {
+			query.map(packageId => qb.orWhere({ package: packageId }))
+		}))
+		.orderBy('p.tier')
+		.getMany()
+
+	if (pkgs.length === 0) {
+		return response.status(404)
+			.json({
+				message: '404 Not Found',
+				error: 'Package not found',
+				date: new Date()
+			})
+	}
+
+	return response.status(200)
+		.json({
+			message: '200 Successful',
+			date: new Date(),
+			count: pkgs.length,
+			data: pkgs.map(data => {
+				const entries = Object.entries(data)
+					.filter(([key]) => key !== 'databaseId' && key !== 'isPruned' && key !== 'isCurrent' && key !== 'repositorySlug')
+
+				return {
+					...Object.fromEntries(entries),
+					refs: {
+						repo: `${$product.api_endpoint}/jailbreak/repository/${data.repositorySlug}`
+					}
+				}
+			})
+		})
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { App, Request, Response } from '@tinyhttp/app'
 
 import * as packageLookup from './router/package/lookup.js'
+import * as packageMultiLookup from './router/package/multiLookup.js'
 import * as packageSearch from './router/package/search.js'
 import * as repositoryLookup from './router/repository/lookup.js'
 import * as repositoryRanking from './router/repository/ranking.js'
@@ -32,7 +33,9 @@ export const http = new App<never, Request, LocalsResponse>({
 })
 
 http.get('/jailbreak/package/search', packageSearch.middleware, packageSearch.handler)
+http.get('/jailbreak/package/multi', packageMultiLookup.middleware, packageMultiLookup.handler)
 http.get('/jailbreak/package/:package', packageLookup.middleware, packageLookup.handler)
+
 http.get('/jailbreak/repository/search', repositorySearch.middleware, repositorySearch.handler)
 http.get('/jailbreak/repository/ranking', repositoryRanking.middleware, repositoryRanking.handler)
 http.get('/jailbreak/repository/:repository', repositoryLookup.middleware, repositoryLookup.handler)
