99b7e871e5277ff2df460c1eb14c4394943d17c2
Aarnav Tale <aarnav@tale.me>
2023-01-20 12:09:43 -0500

    feat: rewrite health endpoint to use actual metrics

--- crates/api/src/routes/utility/health.rs
+++ crates/api/src/routes/utility/health.rs
@@ -1,13 +1,79 @@
+use crate::{
+	db::{prisma, typesense},
+	utility::{http_respond, tokio_run},
+};
+use prisma_client_rust::Raw;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
-use tide::{Request, Result, StatusCode::Ok as OK};
+use tide::{Request, Result};
 
-use crate::utility::json_respond;
+/// Represents the /health response from Typesense
+#[derive(Serialize, Deserialize)]
+struct TypesenseHealth {
+	ok: bool,
+}
 
+/// Represents the output of 'SELECT 1;' from Postgres
+#[derive(Serialize, Deserialize)]
+struct PostgresHealth {
+	#[serde(rename = "?column?")]
+	column: u16,
+}
+
+/// Returns the health of the API
+/// Calculated by checking the health of Typesense and Postgres
 pub async fn health(_req: Request<()>) -> Result {
-	return Ok(json_respond(
-		OK,
+	let (typesense_healthy, postgres_healthy) = tokio_run(async move {
+		let typesense_healthy = match typesense().get("/health").await {
+			Ok(mut response) => {
+				let health: TypesenseHealth = match response.body_json().await {
+					Ok(health) => health,
+					Err(err) => {
+						println!("Failed to serialize Typesense health response: {}", err);
+						TypesenseHealth { ok: false }
+					}
+				};
+
+				health.ok
+			}
+			Err(err) => {
+				println!("Typesense health check failed: {}", err);
+				false
+			}
+		};
+
+		let postgres_healthy = match prisma()
+			._query_raw::<PostgresHealth>(Raw::new("SELECT 1;", vec![]))
+			.exec()
+			.await
+		{
+			Ok(health) => {
+				if health.len() == 1 {
+					health[0].column == 1
+				} else {
+					false
+				}
+			}
+			Err(err) => {
+				println!("Postgres health check failed: {}", err);
+				false
+			}
+		};
+
+		(typesense_healthy, postgres_healthy)
+	});
+
+	let status_code = match typesense_healthy && postgres_healthy {
+		true => 200,
+		false => 500,
+	};
+
+	http_respond(
+		status_code,
 		json!({
-			"status": "OK"
+			"healthy": typesense_healthy && postgres_healthy,
+			"typesense_healthy": typesense_healthy,
+			"postgres_healthy": postgres_healthy,
 		}),
-	));
+	)
 }
