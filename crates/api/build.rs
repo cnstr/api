6d77a8f503f4f355dcdf03e58489cf0e2646ddb2
Aarnav Tale <aarnavtale@icloud.com>
2024-06-20 00:12:28 -0400

    chore: cleanup old deployment flow tooling

--- crates/api/build.rs
+++ crates/api/build.rs
@@ -1,9 +1,12 @@
-use manifest::{load_manifest, Conditional};
 use openapi::{generate_openapi, Metadata};
 use reqwest::ClientBuilder;
 use serde::Deserialize;
 use serde_json::{from_str as from_json, to_string as to_string_json};
-use serde_yaml::to_string as to_string_yaml;
+use serde_yaml::{from_str as from_yaml, to_string as to_string_yaml};
+use std::{
+	fs::{canonicalize, read_to_string},
+	path::Path,
+};
 use tokio::main;
 use vergen::{vergen, Config, ShaKind};
 
@@ -14,6 +17,58 @@ use vergen::{vergen, Config, ShaKind};
 #[warn(clippy::style)]
 #[warn(clippy::complexity)]
 #[warn(clippy::perf)]
+
+/// Strongly-typed manifest
+#[derive(Deserialize)]
+pub struct Manifest {
+	pub meta: Meta,
+	pub build: Build,
+	pub endpoints: Endpoints,
+}
+
+/// Strongly-typed meta section
+#[derive(Deserialize)]
+pub struct Meta {
+	pub code_name: String,
+	pub description: String,
+	pub contact_email: String,
+	pub production_name: String,
+	pub copyright_string: String,
+}
+
+/// Strongly-typed build section
+#[derive(Deserialize)]
+pub struct Build {
+	pub bump: Bump,
+	pub piracy_endpoint: String,
+	pub sentry_dsn: Conditional,
+	pub k8s_control_plane: String,
+	pub postgres_url: Conditional,
+	pub typesense_host: Conditional,
+	pub vector_host: Conditional,
+}
+
+#[derive(Deserialize)]
+pub struct Conditional {
+	pub debug: String,
+	pub release: String,
+}
+
+#[derive(Deserialize)]
+pub struct Bump {
+	pub documentation_id: String,
+	pub access_token: String,
+}
+
+/// Strongly-typed endpoints section
+#[derive(Deserialize)]
+pub struct Endpoints {
+	pub api: String,
+	pub docs: String,
+	pub site: String,
+	pub privacy: String,
+	pub privacy_updated: String,
+}
 
 /// Kubernetes HTTP Response
 #[derive(Deserialize)]
@@ -59,6 +114,33 @@ fn main() {
 		manifest.build.typesense_host,
 		manifest.build.vector_host,
 	);
+}
+
+/// Loads the manifest.yaml file and deserializes it
+/// Panics if the file is not found or if it fails to deserialize
+pub fn load_manifest(path: &str) -> Manifest {
+	let manifest_path = Path::new(path);
+	let manifest = match read_to_string(manifest_path) {
+		Ok(manifest) => {
+			match canonicalize(manifest_path) {
+				Ok(path) => println!("cargo:rerun-if-changed={}", path.display()),
+				Err(e) => panic!("Failed to canonicalize manifest path ({e})"),
+			};
+
+			match from_yaml(&manifest) {
+				Ok(value) => {
+					let value: Manifest = value;
+					value
+				}
+				Err(e) => panic!("Failed to parse manifest.yaml ({e})"),
+			}
+		}
+		Err(e) => {
+			panic!("Failed to read manifest.yaml ({e})")
+		}
+	};
+
+	manifest
 }
 
 /// Registers environment variables from the 'vergen' crate
